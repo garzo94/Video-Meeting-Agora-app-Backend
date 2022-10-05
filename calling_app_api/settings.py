9c3210743b28cdadab80b80790c049c04bc05304
garzo94 <alexgarzo25@gmail.com>
2022-10-05 12:32:58 -0600

    adding db

--- calling_app_api/settings.py
+++ calling_app_api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'calling_app_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fL6BAsRlMyGj9ubYWr9Q',
+        'HOST': 'containers-us-west-73.railway.app',
+        'PORT': '6652',
     }
 }
 
